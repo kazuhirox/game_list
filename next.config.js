7ba3876fa1e9da434d79df4fca5b69a2ad291fd6
Kazuhiro Ohtani <kazuhiro_otani@yayoi-kk.co.jp>
2023-12-28 13:28:24 +0900

    deployの修正

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  reactStrictMode: true,
+  basePath: "/game_list",
+  assetPrefix: "/game_list",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
